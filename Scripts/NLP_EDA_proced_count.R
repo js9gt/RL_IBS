55fab9026690efb738b6e5d91a63d187e0d42c3a
js9gt <js9gt@virginia.edu>
2022-05-18 23:44:56 -0400

    adding comments & trash code

--- Scripts/NLP_EDA_proced_count.R
+++ Scripts/NLP_EDA_proced_count.R
@@ -12,3 +12,28 @@ ord_tab_df <- as.data.frame(my_tab[order(my_tab, decreasing = TRUE)])
 
 ## generating output table
 write_xlsx(ord_tab_df, "/Users/janeshe/Desktop/RL_IBS/Outputs/NLP_EDA_ordered_table.xlsx")
+
+
+### NLP exploration
+
+# library(tokenizers)
+# library(tidytext)
+
+# ## table of frequencies
+# my_tab <- table(ibs_dat[,"PROCEDURE_DESC"]) 
+# 
+# ## order table & convert to DF
+# ord_tab_df <- as.data.frame(my_tab[order(my_tab, decreasing = TRUE)])
+# 
+# write_xlsx(ord_tab_df, "/Users/janeshe/Desktop/RL_IBS/NLP_EDA_ordered_table.xlsx")
+
+
+# ## Create vector just for procedure descriptions for ease of access & turn into phrases of 5 words
+# desc <- tibble(txt = ibs_dat[,"PROCEDURE_DESC"])  %>% 
+#   mutate_all(as.character) %>% 
+#   unnest_tokens(word, txt, token="ngrams", n=5, to_lower = T) %>%
+#   count(word) %>% 
+#   arrange(desc(n))
+#  
+
+```
