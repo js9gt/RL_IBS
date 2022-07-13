b8f16e5235bf1470a6b20633ef3c56aa5a672964
js9gt <js9gt@virginia.edu>
2022-07-13 12:45:21 -0400

    reading in additional 4 patient data

--- Scripts/read_data.R
+++ Scripts/read_data.R
@@ -11,7 +11,18 @@ setwd("/Users/janeshe/Desktop/RL_IBS")
 ibs_dat <- read_excel("ibs_dat.xlsx", skip = 1)
 
 
+#Read in excel file for 4 additional patients
+dat_4pts <- read_excel("data_4pts.xlsx")
+
+
 ## Pad NDC codes to contain 0 in front so all are standardized length
 #### COMEMNT---- turns NDC codes from numeric to a character when including 0's
 
 ibs_dat$ndc <- str_pad(ibs_dat$ndc, max(nchar(na.omit(ibs_dat$ndc))), side="left", pad="0")
+
+## do the same for 4 pts NDC codes
+
+dat_4pts$ndc <- str_pad(dat_4pts$ndc, max(nchar(na.omit(dat_4pts$ndc))), side="left", pad="0")
+
+
+
