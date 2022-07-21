4fd72ca609043b950090a648f95e7315dab4a324
js9gt <js9gt@virginia.edu>
2022-07-21 14:57:17 -0400

    fix data reading error

--- Scripts/read_data.R
+++ Scripts/read_data.R
@@ -12,8 +12,25 @@ ibs_dat <- read_excel("ibs_dat.xlsx", skip = 1)
 
 
 #Read in excel file for 4 additional patients
-dat_4pts <- read_excel("data_4pts.xlsx")
-
+dat_4pts <- read_excel("data_4pts.xlsx", 
+                        col_types = c("text", "numeric", "text", 
+                                       "text", "text", "text", "text", "numeric", 
+                                       "text", "numeric", "numeric", "text", 
+                                       "numeric", "numeric", "numeric", 
+                                       "numeric", "numeric", "text", "text", 
+                                       "text", "text", "numeric", "text", 
+                                       "numeric", "numeric", "text", "text", 
+                                       "text", "text", "text", "text", "text", 
+                                       "text", "text", "numeric", "numeric", 
+                                       "numeric", "numeric", "text", "numeric", 
+                                       "numeric", "numeric", "numeric", 
+                                       "numeric", "numeric", "numeric", 
+                                       "numeric", "numeric", "numeric", 
+                                       "numeric", "numeric", "numeric", 
+                                       "text", "text", "numeric", "numeric", 
+                                       "text", "text", "text", "text", "text", 
+                                       "text", "text", "numeric", "text", 
+                                       "numeric"))
 
 ## Pad NDC codes to contain 0 in front so all are standardized length
 #### COMEMNT---- turns NDC codes from numeric to a character when including 0's
@@ -23,6 +40,7 @@ ibs_dat$ndc <- str_pad(ibs_dat$ndc, max(nchar(na.omit(ibs_dat$ndc))), side="left
 ## do the same for 4 pts NDC codes
 
 dat_4pts$ndc <- str_pad(dat_4pts$ndc, max(nchar(na.omit(dat_4pts$ndc))), side="left", pad="0")
+
 
 
 
