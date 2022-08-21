d74081d0a130fe9882cf462a894a19aa6b401e54
js9gt <js9gt@virginia.edu>
2022-08-21 16:11:34 -0400

    moving to read_data stage

--- Scripts/group_col_labs_imaging_etc.R
+++ Scripts/group_col_labs_imaging_etc.R
@@ -8,107 +8,11 @@ setwd("/Users/janeshe/Desktop/RL_IBS")
 ##Add source file reading in data
 source('Scripts/read_data.R')
 
-
-## Create new column called "group"-- these will have the option to be:
-# Prescriptions
-# Imaging
-# Labs
-# Large Bowel Description
-# Small Bowel Description
-# Ileo-cecal Description
-# Colonoscopy
-
-ibs_dat$group <- 0
-
-## we know prescription claims have NDC codes-- for anything with an NDC code, rewrite that as the "prescription" group
-ibs_grouped <- ibs_dat %>%
-  
-  ## group the labs
-  mutate(group = case_when(
-    ## CBC w/ diff
-    proc_cde ==  "85025" |
-      ## C reactive protein
-      proc_cde == "86140" | 
-      ## Erythrocyte sedimentation rate
-      proc_cde == "85651" | 
-      proc_cde == "85652" |
-      ## Albumin
-      proc_cde == "82040" |
-      ## AST
-      proc_cde == "84450" |
-      ## ALT
-      proc_cde == "84460" |
-      ## Total protein
-      proc_cde == "84155"
-    ~ "labs", 
-    
-    ## group imaging   
-    ## CT abdomen w/ & w/o contrast
-    proc_cde == "74170" | 
-      ## CT absomen & pelvis w/o contrast
-      proc_cde == "74176" | 
-      ## CT absomen & pelvis w/ contrast 
-      proc_cde == "74177" |
-      ## MRI abdomen
-      proc_cde == "74183" |
-      ## MR enterography
-      proc_cde == "74181"|
-      proc_cde == "74182" |
-      proc_cde == "74183"|
-      proc_cde == "72195" |
-      proc_cde == "72196"|
-      proc_cde == "72197" |
-      ## Xray abdomen
-      proc_cde == "74018"
-    ~ "imaging",
-    
-    ## group prescriptions (anything that has a non-NA NDC code)
-    ndc != "NA" ~ "prescriptions",
-    
-    ## group Large Bowel Description
-    proc_cde ==  "44140" |
-      proc_cde == "44141"| 
-      proc_cde == "44143" | 
-      proc_cde == "44144"|
-      proc_cde == "44145" |
-      proc_cde == "44146"|
-      proc_cde == "44147" |
-      proc_cde == "44150"|
-      proc_cde == "44151" |
-      proc_cde == "44155"|
-      proc_cde == "44156" |
-      proc_cde == "44157"|
-      proc_cde == "44158" |
-      proc_cde == "44204"|
-      proc_cde == "44206" |
-      proc_cde == "44207"|
-      proc_cde == "44208" |
-      proc_cde == "44210"|
-      proc_cde == "44211" |
-      proc_cde == "44212"    
-    ~ "large_bowel",
-    
-    ## group Small Bowel Description
-    proc_cde ==  "44120" |
-      proc_cde == "44121"| 
-      proc_cde == "44125" | 
-      proc_cde == "44202"    ~ "small_bowel",
-    
-    ## group ILEO-CECAL Description
-    proc_cde ==  "44160" |
-      proc_cde == "44205" 
-    ~ "ileocecal",
-    ## group colonoscopy
-    proc_cde ==  "45378" |
-      proc_cde == "45380" 
-    ~ "colonoscopy"))
-
-
 #### Create plot with unique "from_dt" as x-axis, number of TOTAL claims as y-axis, color by the group
 ## also facet by year 
 
 ### unique claims & counts per date
-group_claim_cts <- ibs_grouped %>% arrange(ymd(ibs_dat$from_dt)) %>%
+group_claim_cts <- ibs_dat %>% arrange(ymd(ibs_dat$from_dt)) %>%
   group_by(from_dt, claimno, group) %>%
   tally %>% mutate(month_day = format(from_dt, "%m-%d"),
                    year = format(from_dt, "%Y"), month = format(from_dt, "%m"))
